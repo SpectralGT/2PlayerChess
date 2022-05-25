99dfbde1061b2bc151f55e224cf4fa863031a556
SpectralGT <atharvsingh1396@gmail.com>
2022-05-25 02:20:45 -0700

    created square component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import Square from "./components/Square.svelte";
+    import {Piece} from "./Consts";
 </script>
 
-<main>
+<main >
+    <Square piece={Piece.BLACK_PAWN} OnClick={()=>{}} isWhite={false} isPicked={false} isValidDestination={false}/>
 </main>
 
 <style>
- 
 </style>
---
+++ frontend/src/components/Square.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { Piece } from "../Consts";
+
+	export var piece: Piece;
+	export var OnClick = () => {};
+	export var isWhite: boolean;
+	export var isPicked: boolean;
+	export var isValidDestination: boolean;
+
+	const whiteSquareColor = "#EADAB9";
+	const blackSquareColor = "#C2A482";
+	const isPickedColor = "lime";
+	const isValidDestinationColor = "yellow";
+
+	let backgroundColor: string = isWhite ? whiteSquareColor : blackSquareColor;
+	
+	// Special square highlighting a picked square or valid destination
+	if (isPicked) {
+		backgroundColor = isPickedColor;
+	} else if (isValidDestination) {
+		backgroundColor = isValidDestinationColor;
+	}
+</script>
+
+<div
+	style="--background-color:{backgroundColor}"
+	onclick={() => {
+		OnClick();
+	}}
+>
+	{piece}
+</div>
+
+<style>
+	div {
+		background-color: var(--background-color);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		font-size: 5vw;
+	}
+</style>
