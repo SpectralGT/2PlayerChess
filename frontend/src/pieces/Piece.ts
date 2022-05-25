eb10ed4b9a2d9a1d89829127c3c1e701eaa49822
SpectralGT <atharvsingh1396@gmail.com>
2022-05-25 07:07:02 -0700

    create piece class and king class

---
+++ frontend/src/pieces/Piece.ts
@@ -0,0 +1,9 @@
+export default class Piece {
+
+    player:number;
+    style:any;
+    constructor(player:number, iconUrl:string){
+      this.player = player;
+      this.style = {backgroundImage: "url('"+iconUrl+"')"};
+    }
+  }
